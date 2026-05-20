8735b837ca00c78669e92d3e9e211abb3061cc93
Samuel Abreu <samuelabreudev@gmail.com>
2026-05-20 15:32:56 -0300

    feat: Adicionando novas rotas de usuario.

---
+++ api/routes/privateRoutes/usuariosRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router();
+const usuariosController = require('../../controllers/usuariosControler');
+
+
+// CRUD USUARIOS
+router.get('/usuarios', usuariosController.listarUsuarios);
+router.post('/usuarios', usuariosController.criarUsuario);
+// router.put('/usuarios/:id', usuariosController.editarUsuario);
+// router.delete('/usuarios/:id', usuariosController.deletarUsuario);
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const campanhasPrivateRoutes = require('./routes/privateRoutes/campanhasRoutes')
 const imoveisPrivateRoutes = require('./routes/privateRoutes/imoveisRoutes')
 const campanhasPublicRoutes = require('./routes/publicRoutes/campanhasRoutes')
 const imoveisPublicRoutes = require('./routes/publicRoutes/imoveisRoutes')
+const usuariosPublicRoutes = require('./routes/privateRoutes/usuariosRoutes')
 const app = express();
 const PORT = 3000;
 
@@ -12,6 +13,7 @@ app.use(express.json());
 // Rotas Privadas
 app.use('/api', campanhasPrivateRoutes);
 app.use('/api', imoveisPrivateRoutes);
+app.use('/api', usuariosPublicRoutes);
 
 // Rotas Publicas
 app.use('/api', campanhasPublicRoutes);
