3557c2d000e01bb7293487934b8d3f97aa145426
Samuel Abreu <samuelabreudev@gmail.com>
2026-07-18 22:51:29 -0300

    re-init: Recreating all architecture with TypeScript

--- api/routes/publicRoutes/imoveisRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const imoveisController = require('../../controllers/imoveisController');
-
-
-// CRUD IMOVEIS
-router.get('/imoveis', imoveisController.listarImoveis);
-
-module.exports = router;
