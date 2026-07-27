7f5be7a40bcd0f39204da6b5e16c11059108231b
Samuel Abreu <samuelabreudev@gmail.com>
2026-07-27 19:02:56 -0300

    feat: Adicionando configuracoes de associacoes de tabelas

--- api/src/config/associations.ts
+++ api/src/config/associations.ts
@@ -1,13 +1,13 @@
-import Imovel from '../modules/imoveis/imoveis.model';
-import Loteamento from '../modules/loteamentos/loteamentos.model';
-import { ImovelFoto } from '../modules/imoveis/imoveis-fotos.model';
-
-// Associações Imovel ↔ ImovelFoto
-Imovel.hasMany(ImovelFoto, { foreignKey: 'id_imovel', as: 'fotos' });
-ImovelFoto.belongsTo(Imovel, { foreignKey: 'id_imovel', as: 'imovel' });
-
-// Associação Imovel ↔ Loteamento
-Imovel.belongsTo(Loteamento, { foreignKey: 'id_loteamento', as: 'loteamento' });
-Loteamento.hasMany(Imovel, { foreignKey: 'id_loteamento', as: 'imoveis' });
-
-export { Imovel, Loteamento, ImovelFoto };
+import Imovel from '../modules/imoveis/imoveis.model'; 
+import Loteamento from '../modules/loteamentos/loteamentos.model'; 
+import { ImovelFoto } from '../modules/imoveis/imoveis-fotos.model'; 
+ 
+// Associações Imovel ↔ ImovelFoto 
+Imovel.hasMany(ImovelFoto, { foreignKey: 'id_imovel', as: 'fotos' }); 
+ImovelFoto.belongsTo(Imovel, { foreignKey: 'id_imovel', as: 'imovel' }); 
+ 
+// Associação Imovel ↔ Loteamento 
+Imovel.belongsTo(Loteamento, { foreignKey: 'id_loteamento', as: 'loteamento' }); 
+Loteamento.hasMany(Imovel, { foreignKey: 'id_loteamento', as: 'imoveis' }); 
+ 
+export { Imovel, Loteamento, ImovelFoto }; 
